5c8094b46d7b24dd819611a606cb9e839411d135
jeongsoo <dnjawm19@naver.com>
2023-02-07 16:45:16 +0900

    HelloController

--- src/main/java/helloboot/toby/HelloController.java
+++ src/main/java/helloboot/toby/HelloController.java
@@ -1,4 +1,12 @@
 package helloboot.toby;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
 }
