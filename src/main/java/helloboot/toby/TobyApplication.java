f72425c94a3eb2cf576d762fb91adbe6751c64d2
jeongsoo <dnjawm19@naver.com>
2023-02-07 16:53:21 +0900

    웹서버 생성

--- src/main/java/helloboot/toby/TobyApplication.java
+++ src/main/java/helloboot/toby/TobyApplication.java
@@ -2,12 +2,18 @@ package helloboot.toby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @SpringBootApplication
 public class TobyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobyApplication.class, args);
+//        SpringApplication.run(TobyApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
