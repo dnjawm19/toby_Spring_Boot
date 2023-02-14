2084e180254e7526156cf08eb573d43009092745
jeongsoo <dnjawm19@naver.com>
2023-02-14 20:46:41 +0900

    컨트롤러 매핑 & 바인딩

--- src/main/java/helloboot/toby/HelloController.java
+++ src/main/java/helloboot/toby/HelloController.java
@@ -1,6 +1,9 @@
 package helloboot.toby;
 
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HelloController {
     public String hello(String name) {
         return "Hello " + name;
