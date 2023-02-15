4f9c119a5c0eef6b8df290b00ec0229248069c0d
jeongsoo <dnjawm19@naver.com>
2023-02-15 16:05:42 +0900

    스프링 컨테이너 사용

--- src/main/java/helloboot/toby/HelloController.java
+++ src/main/java/helloboot/toby/HelloController.java
@@ -1,9 +1,5 @@
 package helloboot.toby;
 
-
-import org.springframework.stereotype.Controller;
-
-@Controller
 public class HelloController {
     public String hello(String name) {
         return "Hello " + name;
