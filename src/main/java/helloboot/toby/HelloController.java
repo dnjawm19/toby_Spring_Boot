d3464aa560d274f1a1bbc02dcab44fc473658e9f
jeongsoo <dnjawm19@naver.com>
2023-02-16 17:50:17 +0900

    Component 어노테이션

--- src/main/java/helloboot/toby/HelloController.java
+++ src/main/java/helloboot/toby/HelloController.java
@@ -1,12 +1,10 @@
 package helloboot.toby;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping("/hello")
 @RestController
 public class HelloController {
     private final HelloService helloService;
@@ -15,7 +13,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/helloboot/toby/SimpleHelloService.java
+++ src/main/java/helloboot/toby/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package helloboot.toby;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name){
