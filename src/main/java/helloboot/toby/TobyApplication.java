a8f65082921dee2727be8886fb6cb23b1ad86217
jeongsoo <dnjawm19@naver.com>
2023-02-28 17:47:31 +0900

    SpringBootApplication 작동원리

--- src/main/java/helloboot/toby/TobyApplication.java
+++ src/main/java/helloboot/toby/TobyApplication.java
@@ -1,12 +1,10 @@
 package helloboot.toby;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,23 +22,6 @@ public class TobyApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(TobyApplication.class, args);
-
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext ->
-                        servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                                .addMapping("/*"));
-                webServer.start();
-            }
-        };
-        applicationContext.register(TobyApplication.class);
-        applicationContext.refresh();
+        MySpringApplication.run(TobyApplication.class, args);
     }
 }
