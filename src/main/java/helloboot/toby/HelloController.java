13ce370fc2baed8796df351eef758ce6d781b38c
jeongsoo <dnjawm19@naver.com>
2023-03-06 19:46:08 +0900

    단위 테스트 코드 작성

--- src/main/java/helloboot/toby/HelloController.java
+++ src/main/java/helloboot/toby/HelloController.java
@@ -1,8 +1,5 @@
 package helloboot.toby;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +8,9 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
-
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
--- src/main/java/helloboot/toby/SimpleHelloService.java
+++ src/main/java/helloboot/toby/SimpleHelloService.java
@@ -1,8 +1,8 @@
 package helloboot.toby;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name){
